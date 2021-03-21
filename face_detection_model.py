81ef7c3245c7e627d1faecf88ba503b47a8873e5
Wil-conn <wilconnolly@cmail.carleton.ca>
2021-03-21 14:57:01 -0400

    starter code to build on

---
+++ face_detection_model.py
@@ -0,0 +1,58 @@
+from facenet_pytorch import MTCNN
+import numpy as np
+import cv2 as cv
+import time
+
+#trackbar requires a callback function even though we aren't actually going to be using the callback function in the program so we create an empty callback function
+def nothing(x):
+    pass
+
+class face_detect:
+    #f is the factor the MTCNN uses, images is a list of images we want to allign and mode is used to designate which facial feature we want to allign on
+    def __init__(self, f, images, mode):
+        self.mtcnn = MTCNN(factor = f)
+        self.images = images
+        self.mode = mode
+        self.landmarks = {} #dictionary holding the landmarks (i.e eyes location, mouth location, nose location etc.) of every photo. to get the landmarks of the first photo, use key "0"
+
+    def detect(self):
+        for idx, image in np.ndenumerate(self.images):
+            boxes, probs, landmarks = self.mtcnn.detect(image, landmarks = True)
+            self.landmarks[str(idx[0])] = landmarks #not entirely sure why idx is a tuple in the form (index , ). using idx[0] fixes this by just getting the index number
+        if boxes is None:
+            return False
+
+        print(self.landmarks.get("0"))
+        #self.landmarks = landmarks
+        '''
+        if self.mode == 'eyes':
+            angle_of_eyes_tilt
+        '''
+
+#    def angle_of_eyes_tilt(left_eye_coord, right_eye_coord):
+
+    def display(self):
+        idx = 0
+        cv.namedWindow('slideshow')
+        trackbar_name = 'image # %d' % idx
+        cv.createTrackbar(trackbar_name, 'slideshow', 0, len(self.images), nothing)
+        '''
+        for idx, image in np.ndenumerate(self.images):
+            cv.imshow('image' + str(idx), image)
+        '''
+        while idx < len(self.images):
+            print(idx)
+            img = self.images[cv.getTrackbarPos(trackbar_name, 'slideshow')]
+
+            height = 600
+            #dim = (int(height/self.images[idx].shape[0] * self.images[idx].shape[1]), height)
+            #img = cv.resize(self.images[idx], dim)
+            #cv.imshow('image', self.images[idx])
+            cv.imshow('slideshow', img)
+            k = cv.waitKey(500)
+
+            #idx += 1
+
+
+
+
---
+++ main.py
@@ -0,0 +1,36 @@
+import torch
+import cv2 as cv
+import numpy as np
+import os
+import sys
+from face_detection_model import *
+
+def get_photos(path):
+    path, dirs, files = next(os.walk(path))
+    file_count = len(files)
+
+    images = np.empty(file_count, dtype = object)
+    c = 0
+    for entry in os.scandir(path):
+        if entry.path.endswith(".jpg") and entry.is_file():
+            print(str(entry.path) + " is picture number " + str(c) + " in the images array")
+            images[c] = cv.imread(str(entry.path))
+            c += 1
+    return images
+
+def main():
+    #mtcnn = MTCNN(factor = 0.709)
+    print(sys.argv[1])
+    path = 'images/'
+    fd = face_detect(0.709, get_photos(sys.argv[1]), 'eyes')
+    fd.detect()
+    fd.display()
+
+    #faces = detect(img, mtcnn)
+    #cv.imshow('image', faces)
+    #print(fd.landmarks)
+
+
+if __name__ == '__main__':
+    main()
+    cv.destroyAllWindows()
